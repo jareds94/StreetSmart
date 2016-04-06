ee75a07aac4c034f11b3656bd420e0eede747345
Jared Schwalbe <jareds94@vt.edu>
2016-04-06 14:04:47 -0400

    Stop tracking dist/ build/ and nbproject/

--- build/web/resources/js/global.js
+++
@@ -1,8 +0,0 @@
-// Fired when DOM is finished loading
-$(document).ready(function() {
-    // Header button hover fix for mobile
-    $(".header-btn").bind("touchstart touchend mouseover mouseout", function(e) {
-        e.preventDefault();
-        $(this).toggleClass("header-btn-hover");
-    });
-});
