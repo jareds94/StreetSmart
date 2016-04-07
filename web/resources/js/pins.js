c43908fe7b26f54c7e4db023bceb73d50ea5fb70
tstreetcs <tstreet@vt.edu>
2016-04-07 12:57:12 -0400

    Initial commit via SourceTree

---
+++ web/resources/js/pins.js
@@ -0,0 +1,18 @@
+/* 
+ * Created by Timothy Street on 2016.04.07  * 
+ * Copyright © 2016 Timothy Street. All rights reserved. * 
+ */
+
+/*
+function sendPinParameters() {
+    passToJSFManagedBean ([ {
+                  latitude : 1,
+                  longitude : 1
+                 } 
+                 ]);  
+}
+
+var pin = // You set your JSON here.
+$("input[id='verticesHiddenContainer'").val(vertices);
+ */
+
