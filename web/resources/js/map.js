b568b475b6cfb07ec0142f7826b6d4e313c82bff
Mukund Katti <mukund23@vt.edu>
2016-04-07 13:45:07 -0400

    added blue dot to show user's current location. need to make it pulsate

--- web/resources/js/map.js
+++ web/resources/js/map.js
@@ -179,6 +179,23 @@ function initialize() {
         "This is another example pin. I'm running out of " +
         "example text to write."
     );
+    var myloc = new google.maps.Marker({
+    clickable: false,
+    icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+                                                    new google.maps.Size(22,22),
+                                                    new google.maps.Point(0,18),
+                                                    new google.maps.Point(11,11)),
+    shadow: null,
+    zIndex: 999,
+    map: map// your google.maps.Map object
+    });
+
+if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function(pos) {
+    var me = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+    myloc.setPosition(me);
+}, function(error) {
+    // ...
+});
 }
 
 // Display a message at the top of the map
