f7719b4792eade2d77f13cb336132d8e32b5fa79
Hung Vu <hungtvu@vt.edu>
2016-04-07 11:55:38 -0400

    Base source packages

---
+++ Source Packages/com/streetsmart/sessionbeanpackage/UserFacade.java
@@ -0,0 +1,31 @@
+/*
+ * Created by Hung Vu on 2016.04.07  * 
+ * Copyright Â© 2016 Hung Vu. All rights reserved. * 
+ */
+package com.streetsmart.sessionbeanpackage;
+
+import com.streetsmart.entitypackage.User;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Kevin
+ */
+@Stateless
+public class UserFacade extends AbstractFacade<User> {
+
+    @PersistenceContext(unitName = "StreetSmartPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UserFacade() {
+        super(User.class);
+    }
+    
+}
