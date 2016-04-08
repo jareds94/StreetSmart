50d9cb83cde67e93c9e84b0ecff2e15a5f537c0e
Jared Schwalbe <jareds94@vt.edu>
2016-04-07 21:24:48 -0400

    SS-16 Finished page layout and design for SignIn.xhtml

--- web/resources/js/global.js
+++ web/resources/js/global.js
@@ -5,4 +5,24 @@ $(document).ready(function() {
         e.preventDefault();
         $(this).toggleClass("header-btn-hover");
     });
+    
+    positionFooter();
 });
+
+// Fired when window is resized
+$(window).resize(function () {
+    positionFooter();
+});
+
+function positionFooter() {
+    // Footer positioning
+    if ($("body").height() < $(window).height()) {
+        $("#footer-wrapper").css("position", "absolute");
+        $("#footer-wrapper").css("bottom", "0px");
+        $("#page-bottom-padding").css("height", "120px");
+    } else {
+        $("#footer-wrapper").css("position", "relative");
+        $("#footer-wrapper").css("bottom", "");
+        $("#page-bottom-padding").css("height", "60px");
+    }
+}
