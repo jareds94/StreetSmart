5beac2938a77fb0949c3f14ab9179c359bc8ae2e
Jared Schwalbe <jareds94@vt.edu>
2016-04-14 13:33:56 -0400

    Added a welcome page

---
+++ web/resources/js/welcome.js
@@ -0,0 +1,25 @@
+// Fired when DOM is finished loading
+$(document).ready(function () {
+    // Hide home button
+    $("#header-links-form\\:home-btn").hide();
+    
+    resizeWelcomeMap($(window).width(), $(window).height() - 130, 0);
+    
+    // Adjust welcome
+    $("#welcome-wrapper").height();
+});
+
+// Fired when window is resized by the user
+$(window).resize(function () {
+    resizeWelcomeMap($(window).width(), $(window).height() - 130, 100);
+});
+
+// Adjust the heights and width to be window dependent
+function resizeWelcomeMap(width, height, delay) {
+    setTimeout(function () {
+        $("#content-container").css("height", height + "px");
+        $("#welcome-wrapper").css("height", height + "px");
+        $("#welcome-title").css("margin-top", height/4 + "px");
+        $("#welcome-map").css("height", height + "px");
+    }, delay);
+}
