fec5aad3f8fae44819c667154dec71add2ef0acc
Jared Schwalbe <jareds94@vt.edu>
2016-04-15 00:42:21 -0400

    Fixed orientation message so it only appear on mobile.

--- web/resources/js/global.js
+++ web/resources/js/global.js
@@ -1,4 +1,12 @@
-// Fired when DOM is finished loading
 $(document).ready(function() {
-    
+    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) && window.orientation !== 0) {
+        alert("For the best experience, please use our website in landscape mode.")
+    }
 });
+
+// Listen for orientation changes
+window.addEventListener("orientationchange", function() {
+    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) && window.orientation !== 0) {
+        alert("For the best experience, please use our website in landscape mode.")
+    }
+}, false);
