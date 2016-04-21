de926a8648692e3da6f90ef9168a1daba610fae6
Michael Bulgakov <mybu5@vt.edu>
2016-04-21 14:20:19 -0400

    Added upload photo func to pin

--- src/java/com/streetsmart/managers/Constants.java
+++ src/java/com/streetsmart/managers/Constants.java
@@ -10,8 +10,8 @@ package com.streetsmart.managers;
  */
 public class Constants {
     public static final String TEMP_FILE = "tmp_file";
-  //  public static final String ROOT_DIRECTORY = "/Users/Kevin/StreetSmartPhotoStorage/";
-    public static final String ROOT_DIRECTORY = "/home/cs4984/StreetSmart/StreetSmartPhotoStorage";
+//    public static final String ROOT_DIRECTORY = "/Users/mybu/StreetSmartPhotoStorage/";
+    public static final String ROOT_DIRECTORY = "/home/cs4704/StreetSmart/StreetSmartPhotoStorage";
 
     public static final Integer THUMBNAIL_SZ = 200;
 
