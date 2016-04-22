55772be283e5dbb897570b1141b9d6bfb22ceee3
Jared Schwalbe <jareds94@vt.edu>
2016-04-22 19:53:04 -0400

    SS-75 This might work but I need jupiter to test it.

--- web/resources/js/map.js
+++ web/resources/js/map.js
@@ -181,6 +181,8 @@ function initialize() {
         if (this.expand) {
             this.div.firstChild.click();
         }
+        
+        removeParam("id");
     };
 
     // Remove pin from map
@@ -329,3 +331,24 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
+
+function removeParam(parameter)
+{
+    var url=document.location.href;
+    var urlparts= url.split('?');
+
+    if (urlparts.length>=2)
+    {
+        var urlBase=urlparts.shift(); 
+        var queryString=urlparts.join("?"); 
+
+        var prefix = encodeURIComponent(parameter)+'=';
+        var pars = queryString.split(/[&;]/g);
+        for (var i= pars.length; i-->0;)               
+            if (pars[i].lastIndexOf(prefix, 0)!==-1)   
+                pars.splice(i, 1);
+        url = urlBase+'?'+pars.join('&');
+        window.history.pushState('',document.title,url);
+    }
+    return url;
+}
