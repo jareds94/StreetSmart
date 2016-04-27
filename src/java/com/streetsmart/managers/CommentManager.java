e5ca0821a6c066e3a54880a2226c3d4f594bbdb2
Mukund Katti <mukund23@vt.edu>
2016-04-27 12:39:08 -0400

    commentManager has been documented.

--- src/java/com/streetsmart/managers/CommentManager.java
+++ src/java/com/streetsmart/managers/CommentManager.java
@@ -10,11 +10,9 @@ import com.streetsmart.entitypackage.User;
 import com.streetsmart.sessionbeanpackage.CommentFacade;
 import com.streetsmart.sessionbeanpackage.PinFacade;
 import com.streetsmart.sessionbeanpackage.UserFacade;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
@@ -31,10 +29,16 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+/**
+ *
+ * @author kattim
+ */
 @Named(value = "commentsManager")
 @SessionScoped
 /**
- *
+ *Session-scoped bean that allows the application to retrieve and store
+ * a pin's comments  for view on the front-end. Also stores currently
+ *  logged in User information and the selected pin.
  * @author kattim
  */
 public class CommentManager implements Serializable {
@@ -74,68 +78,134 @@ public class CommentManager implements Serializable {
     @EJB
     private CommentFacade commentsFacade;
 
+    /**
+     * selectedPinId is initialized to 0 in the constructor.
+     */
     public CommentManager() {
         selectedPinId = 0;
     }
 
+    /**
+     * Gets commentsFacade.
+     * @return
+     */
     public CommentFacade getCommmentsFacade() {
         return commentsFacade;
     }
 
+    /**
+     * Sets commentsFacade.
+     * @param commentsFacade
+     */
     public void setCommentsFacade(CommentFacade commentsFacade) {
         this.commentsFacade = commentsFacade;
     }
 
+    /**
+     * Sets the selected user who is logged on.
+     * @param user
+     */
     public void setSelectedUser(User user) {
         selectedUser = userFacade.findByUserId(userId);
     }
 
+    /**
+     * Gets the selected user who is logged in.
+     * @return
+     */
     public User getSelectedUser() {
         selectedUser = userFacade.findByUserId(userId);
         return selectedUser;
     }
 
+    /**
+     * Sets the selected pin
+     * @param pin
+     */
     public void setSelectedPin(Pin pin) {
         selectedPin = pin;
     }
 
+    /**
+     * Gets the selected pin
+     * @return
+     */
     public Pin getSelectedPin() {
         return selectedPin;
     }
 
+    /**
+     * Sets the selected pin's id, then resets the comment list for that pin.
+     * @param id
+     */
     public void setSelectedPinId(int id) {
         this.selectedPinId = id;
         setCommentsList(commentsFacade.findAllCommentsByPinId(id));
     }
 
+    /**
+     * Gets the selected pin's id
+     * @return
+     */
     public int getSelectedPinId() {
         return selectedPinId;
     }
 
+    /**
+     * Sets the time posted for the comment
+     * @param timePosted
+     */
     public void setTimePosted(int timePosted) {
         this.timePosted = timePosted;
     }
 
+    /**
+     * Gets the time posted 
+     * @return
+     */
     public int getTimedPosted() {
         return timePosted;
     }
 
+    /**
+     * Sets the comment entered.
+     * @param comment
+     */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
+    /**
+     * Gets the comment entered
+     * @return
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     * Sets the user id for the logged in user
+     * @param id
+     */
     public void setUserId(int id) {
         userId = id;
     }
 
+    /**
+     * Gets the userId for the logged in user.
+     * @return
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * Creates a comment, sets all the fields, adds to the database, and sends
+     * an email to the user who posted the pin.
+     * @return
+     * @throws MessagingException
+     * @throws IOException
+     */
     public String createComment() throws MessagingException, IOException {
 
         int timestamp = (int) (new Date().getTime() / 1000);
@@ -161,10 +231,19 @@ public class CommentManager implements Serializable {
         return "";
     }
 
+    /**
+     * Sets the comment list for the pin/
+     * @param comments
+     */
     public void setCommentsList(List<Comment> comments) {
         this.commentsList = comments;
     }
 
+    /**
+     * Gets the comment list for the selected pin, and sets the user ids for
+     * the comments and quicksorts the comments list by time posted.
+     * @return
+     */
     public List<Comment> getCommentsList() {
         commentsList = commentsFacade.findAllCommentsByPinId(selectedPinId);
         if (commentsList == null) {
@@ -183,7 +262,7 @@ public class CommentManager implements Serializable {
     }
 
     /**
-     *
+     * Quicksort of the commentList by time posted.
      * @param low
      * @param high
      */
@@ -232,7 +311,7 @@ public class CommentManager implements Serializable {
     }
 
     /**
-     *
+     * Gets a formatted date from timePosted
      * @param comment
      * @return
      */
@@ -241,6 +320,13 @@ public class CommentManager implements Serializable {
         return format.format(new Date(((long) comment.getTimePosted()) * 1000L));
     }
 
+    /**
+     * Sends an email from the official StreetSmart gmail account to the user
+     * who posted the pin, notifying them that a comment has been posted on the
+     * pin.
+     * @throws MessagingException
+     * @throws IOException
+     */
     public void sendEmail() throws MessagingException, IOException {
         Pin pin = pinFacade.findPinWithId(selectedPinId);
         User postedUser = userFacade.findByUserId(pin.getUserId());
@@ -279,6 +365,12 @@ public class CommentManager implements Serializable {
 
     }
     
+    /**
+     * Returns an emailToString for the email body. 
+     * @param pin
+     * @param commentedUser
+     * @return
+     */
     public String emailToString(Pin pin, User commentedUser){
         
         StringBuilder sb = new StringBuilder();
